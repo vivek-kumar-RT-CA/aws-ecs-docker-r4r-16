ba59574b0b9dc47b5d2fed5bfd032438074f8787
vivek kumar <142480742+vivek-kumar-RT-CA@users.noreply.github.com>
2024-07-18 02:05:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Welcome to my app!!!!!!");
+  res.send("Welcome to my home!!!!!!");
 });
 
 app.get("/info", (req, res) => {
